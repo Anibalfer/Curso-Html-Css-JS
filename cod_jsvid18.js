a9f587a78b7e8eeae04d4217fd7409bcd3c5667b
Anibalfer <80550896+Anibalfer@users.noreply.github.com>
2023-03-19 23:00:54 -0500

    Clases sobre insercion y manipulacion de videos Vid17-19

---
+++ cod_jsvid18.js
@@ -0,0 +1,51 @@
+//declaracion de variables
+let Mivideo, reproducir, barra, progres, maximo;
+maximo=600;
+
+//funcion para iniciar las variables y seguir los eventos
+function comenzar(){
+Mivideo=document.getElementById("Mivideo");
+barra=document.getElementById("barra");
+progres=document.getElementById("progres");
+reproducir=document.getElementById("reproducir");
+
+//Escuchando el evento de reproduccion
+reproducir.addEventListener("click",clicando,false);
+
+barra.addEventListener("click",adelantando,false);
+
+}
+
+function clicando(){
+
+    if((Mivideo.paused==false) && (Mivideo.ended==false)) {
+        Mivideo.pause();
+        reproducir.innerHTML="Play";
+        
+
+    } else {
+
+        Mivideo.play();
+        reproducir.innerHTML="Pause";
+        bucle=setInterval(estado,500);
+    }
+}
+
+function estado(){
+    if (Mivideo.ended==false) {
+        let total=parseInt(Mivideo.currentTime*maximo/Mivideo.duration);
+        progres.style.width=total+"px";
+    }
+
+}
+
+function adelantando(posicion){
+    if ((Mivideo.paused==false) && (Mivideo.ended==false)){;
+    let ratonX=posicion.pageX-barra.offsetLeft;
+    let nuevoTiempo=ratonX*Mivideo.duration/maximo;
+    Mivideo.currentTime=nuevoTiempo;
+    progres.style.width=ratonX+"px";
+    }
+}
+window.addEventListener("load",comenzar,false);
+
