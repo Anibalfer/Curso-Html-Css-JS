d55eadd4617541fb91d6fa18c2ed2e6ae02b876f
Anibalfer <80550896+Anibalfer@users.noreply.github.com>
2023-03-25 23:38:49 -0500

    Primera animacion simple con JS

--- codigojs_vid37.js
+++ codigojs_vid37.js
@@ -1,7 +1,8 @@
-let y=50;
+let y=200;
 let estado=0;
-let ciclo=0;
-let altu=0;
+let altu=y;
+let hor=0;
+
 function comenzar(){
     let elemento=document.getElementById("lienzo");
     lienzo=elemento.getContext("2d");
@@ -16,26 +17,29 @@ function animacion(){
         if((y<850 && estado==0)){
         lienzo.clearRect(0,0,1200,900);
         lienzo.beginPath();
-        lienzo.arc(500,y,40,0,Math.PI*2,false);
+        lienzo.arc(100+hor,y,40,0,Math.PI*2,false);
         lienzo.fill();
         y+=5;
+        hor+=1;
         } 
 
         if(y==850){
+            altu+=50;
             estado=1;
         }
 
-        if(y>=300 && estado==1){
+        if(y>=altu && estado==1){
         lienzo.clearRect(0,0,1200,900);
         lienzo.beginPath();
-        lienzo.arc(500,y,40,0,Math.PI*2,false);
+        lienzo.arc(100+hor,y,40,0,Math.PI*2,false);
         lienzo.fill();
         y-=5
+        hor+=1;
         }
 
-        if(y==300 && ciclo<3){        
+        if(y==altu){        
         estado=0;
-        ciclo+=1
+        
         }
 
        /*  if(y<300 && estado==0){
